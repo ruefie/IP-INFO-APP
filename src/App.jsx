e7cc28da30a767b1dbf857b947e05edbba951a7a
Ruchie Roell <dj_ruchie@yahoo.com.ph>
2024-06-03 19:35:30 +0200

    updated css and added animated icon

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { DateTime } from "luxon";
 import "leaflet/dist/leaflet.css";
 import "antd/dist/reset.css";
-// import 'antd/dist/antd.css'; // Import Ant Design CSS
 import { Layout, Card, Spin, Typography } from "antd";
+import L from "leaflet";
 import "./App.css";
 
 const { Header, Content } = Layout;
@@ -50,53 +50,62 @@ function App() {
     location: { city, country, lat, lng, timezone },
   } = ipInfo;
 
+  const animatedIcon = new L.Icon({
+    iconUrl: "/wired-flat-18-location-pin.gif", // Path to your animated icon
+    iconSize: [50, 50], // Size of the icon
+    iconAnchor: [25, 50], // Point of the icon which will correspond to marker's location
+    popupAnchor: [0, -50] // Point from which the popup should open relative to the iconAnchor
+  });
+
   return (
     <Layout className="layout">
-      <Header>
-        <Title style={{ color: "white" }}>IP Information</Title>
+      <Header className="header">
+        <Title>IP Information</Title>
       </Header>
       <Content className="content">
-        <Card title="IP Information" style={{ width: "80%" }}>
-          <p>
-            <Text strong>IP Address:</Text> {ip}
-          </p>
-          <p>
-            <Text strong>Location:</Text> {city}, {country}
-          </p>
-          <p>
-            <Text strong>Latitude:</Text> {lat}, <Text strong>Longitude:</Text>{" "}
-            {lng}
-          </p>
-          <p>
-            <Text strong>Timezone:</Text> {timezone}
-          </p>
-          <p>
-            <Text strong>Local Time:</Text>{" "}
-            {DateTime.now().setZone(timezone).toFormat("ff")}
-          </p>
-          <p>
-            <Text strong>Country Flag:</Text>
-            <br />
-            <img
-              src={countryInfo.flags.svg}
-              alt={`Flag of ${countryInfo.name.common}`}
-              width="100"
-            />
-          </p>
-        </Card>
-        <Card title="Map" style={{ width: "90%" }}>
+        <div className="ip-info-container">
+          <Card title="IP Information">
+            <p>
+              <Text strong>IP Address:</Text> {ip}
+            </p>
+            <p>
+              <Text strong>Location:</Text> {city}, {country}
+            </p>
+            <p>
+              <Text strong>Latitude:</Text> {lat}, <Text strong>Longitude:</Text>{" "}
+              {lng}
+            </p>
+            <p>
+              <Text strong>Timezone:</Text> {timezone}
+            </p>
+            <p>
+              <Text strong>Local Time:</Text>{" "}
+              {DateTime.now().setZone(timezone).toFormat("ff")}
+            </p>
+            <p>
+              <Text strong>Country Flag:</Text>
+              <br />
+              <img
+                src={countryInfo.flags.svg}
+                alt={`Flag of ${countryInfo.name.common}`}
+                width="100"
+              />
+            </p>
+          </Card>
+        </div>
+        <div className="map-container">
           <MapContainer center={[lat, lng]} zoom={13} className="map">
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
-            <Marker position={[lat, lng]}>
+            <Marker position={[lat, lng]} icon={animatedIcon}>
               <Popup>
                 {city}, {country}
               </Popup>
             </Marker>
           </MapContainer>
-        </Card>
+        </div>
       </Content>
     </Layout>
   );
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {}
+  }
 });
